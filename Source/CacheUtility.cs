c06bcf19d2db46720c447d0e4fcb9f672d5def3d
Zhuma5 <89242390+Zhuma5@users.noreply.github.com>
2026-06-24 18:01:31 +0300

    Change error logs to warning logs in CacheUtility

--- Source/CacheUtility.cs
+++ Source/CacheUtility.cs
@@ -206,7 +206,7 @@ namespace MGAutoSell
             }
             catch (Exception ex)
             {
-                Log.Error($"Failed to PawnGen '{thing.defName}' def\n{ex.Message}\n{ex.StackTrace}");
+                Log.Warning($"Failed to PawnGen '{thing.defName}' def\n{ex.Message}\n{ex.StackTrace}");
                 return null;
             }
         }
@@ -220,7 +220,7 @@ namespace MGAutoSell
             }
             catch (Exception ex)
             {
-                Log.Error($"Failed to ThingGen '{thing.defName}' def\n{ex.Message}\n{ex.StackTrace}");
+                Log.Warning($"Failed to ThingGen '{thing.defName}' def\n{ex.Message}\n{ex.StackTrace}");
                 return null;
             }
         }
